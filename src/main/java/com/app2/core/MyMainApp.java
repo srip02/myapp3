7b67e4603825f587820b527411bb7b3750de775c
srip02 <testautom2017@gmail.com>
2018-07-31 08:44:49 -0400

    initial version

---
+++ src/main/java/com/app2/core/MyMainApp.java
@@ -0,0 +1,13 @@
+package com.app2.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyMainApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
